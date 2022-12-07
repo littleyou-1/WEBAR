e857ae1a00d4f162692e9267f92b825d137b21f6
littleyou-1 <53014164+littleyou-1@users.noreply.github.com>
2022-12-07 06:18:17 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -65,13 +65,13 @@ butFront.addEventListener('click', function() {
             } else {
               if (a===7) {
                 console.log(a);
-                document.querySelector("#arBox").setAttribute("src", "./assets/PatronUs.glb");
-                document.querySelector("#arBox").setAttribute("ios-src", "./assets/PatronUs.usdz");
+                document.querySelector("#arBox").setAttribute("src", "./assets/Patron.glb");
+                document.querySelector("#arBox").setAttribute("ios-src", "./assets/Patron.usdz");
             } else {
               if (a===8) {
                 console.log(a);
-                document.querySelector("#arBox").setAttribute("src", "./assets/PatronUs.glb");
-                document.querySelector("#arBox").setAttribute("ios-src", "./assets/PatronUs.usdz");
+                document.querySelector("#arBox").setAttribute("src", "./assets/Patron.glb");
+                document.querySelector("#arBox").setAttribute("ios-src", "./assets/Patron.usdz");
             } else {
               if (a===9) {
                 console.log(a);
